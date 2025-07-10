5d9cdaae0b9e0c385cb1368ede50badff7cbf26b
vagu sri gana satvik <srigana02@gmail.com>
2025-07-10 23:50:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "writer_public_ip" {
   description = "Public IP of the writer EC2 instance"
-  value       = aws_instance.aws_dev.public_ip
+  value       = aws_instance.writer_instamce/publoc.public_ip
 }
 
 output "reader_public_ip" {
